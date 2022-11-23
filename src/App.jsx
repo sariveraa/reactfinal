bf19a053cafddd1b862ef8513edc1d7f64021163
Salomé Rivera <sariveraa@unal.edu.co>
2022-11-23 12:19:47 -0500

    Se sube proyecto pero sale un error al correr la pagina

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Inicio from "./Inicio";
+import Tabla from "./Tabla";
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+export function app() {
+  return (
+    <div>
+      <p>Parcial final</p>
+    </div>
+  );
+}
+
---
+++ src/Inicio.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+export function Inicio() {
+  return (
+    <div>
+      <div id="carouselExampleSlidesOnly" className="carousel slide" data-bs-ride="carousel">
+        <div className="carousel-inner">
+          <div className="carousel-item active">
+            <img src="..." className="d-block w-100" alt="..." />
+          </div>
+          <div className="carousel-item">
+            <img src="..." className="d-block w-100" alt="..." />
+          </div>
+          <div className="carousel-item">
+            <img src="..." className="d-block w-100" alt="..." />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
